95d2d7cd2c1b8b808393e7c33317688b54cc5baa
Nithack <andreynithack@gmail.com>
2021-07-20 00:32:36 -0300

    feat: delete produto

--- src/services/produtos.service.js
+++ src/services/produtos.service.js
@@ -43,6 +43,22 @@ class ProdutosService {
       throw new Error({ 'ProductService error: ': error.message });
     }
   }
+  // delete product
+  async delete(id) {
+    try {
+      // busca o produto
+      const findProduto = await this.dbProduto.findByPk(id)
+      // verifica se encontrou o produto
+      if(!findProduto) return null
+      // deleta o produto
+      return await findProduto.destroy();
+    } catch (error) {
+      // mostra o erro no console
+      console.log(error);
+      // gera um erro
+      throw new Error({ 'ProdutosService error: ': error.message });
+    }
+  }
 }
 // instancia o produtosService
 const produtosService = new ProdutosService();
