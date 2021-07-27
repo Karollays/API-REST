672251ebfabe4433bbe6706fb257fffbe77a7c1c
Nithack <andreynithack@gmail.com>
2021-07-27 00:24:32 -0300

    fix: edit port

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -7,7 +7,7 @@ class ExpressConfig {
   // express configuration 
   constructor() {
     this.app = express();
-    this.port = process.env.APP_PORT || 3000;
+    this.port = process.env.PORT || 3000;
     this.host = process.env.APP_HOST || '0.0.0.0';
     this.loadExpressUse()
     this.dbConfig = sequelize
