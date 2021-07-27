715b6e0ffde449994782351f566c5bf4db18dbeb
Karollays <karol.penteadoalves@gmail.com>
2021-07-27 15:30:04 -0300

    feat: adicionando Post

--- src/services/clientes.service.js
+++ src/services/clientes.service.js
@@ -14,6 +14,21 @@ class ClientesService {
       throw new Error({ 'ClientesService error: ': error.message });
     }
   }
+  // post cliente
+  async create(cliente) {
+    try {
+      // instancia um novo cliente
+      const newCliente = new Cliente(cliente);
+      // salva o cliente no banco
+      console.log(newCliente)
+      return await newCliente.save();
+    } catch (error) {
+      // mostra o erro no console
+      console.log(error.message);
+      // gera um erro
+      throw new Error('ClienteService error: ' + error.message);
+    }
+  }
   
 }
 // instancia o clientesService
