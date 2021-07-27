6441fc05563ac05d889e2eaae6ba031ed6a6ec46
Nithack <andreynithack@gmail.com>
2021-07-26 23:40:54 -0300

    fix: config postgres connection

--- src/config/sequelize.config.js
+++ src/config/sequelize.config.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 class SequelizeConfig {
   constructor() {
     // sequelize connection
-    this.database = new Sequelize(process.env.DATABASE_URL,
-      {
-        dialect: 'postgres',
-      }
-    );
+    this.database = new Sequelize(process.env.DATABASE_URL,{
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+    });
   }
   async startDatabase() {
     // start db connection
@@ -18,7 +23,7 @@ class SequelizeConfig {
       console.log('Conexão com banco de dados estabelecida.');
     } catch (error) {
       // log de conexão com o banco de dados com erro
-      console.error('Erro ao conectar ao banco de dados:', error.message);
+      console.error('Erro ao conectar ao banco de dados:', error);
     }
   }
 }
