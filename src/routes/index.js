28232426d7ac87af2e435478a37208ef9298c951
Nithack <andreynithack@gmail.com>
2021-07-29 01:49:41 -0300

    refactor: deletando arquivos não utilizados

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,9 @@ const comprasRouter = require('./compras.router')
 const produtosRouter = require('./produtos.router')
 const pedidosRouter = require('./pedidos.router')
 const funcionariosRouter = require('./funcionarios.router')
-const fornecedoresRouter = require('./fornecedores.router')
 
 appRouter.use('/clientes', clientesRouter)
 appRouter.use('/compras', comprasRouter)
-appRouter.use('/fornecedores', fornecedoresRouter)
 // uso da rota de produtos
 appRouter.use('/produtos', produtosRouter)
 appRouter.use('/funcionarios', funcionariosRouter)
