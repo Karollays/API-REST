c3ce886f2ea36d6875d6190d57d595810f3cdc29
bianca-scharneski <biancascharneski@gmail.com>
2021-07-28 21:11:53 -0300

    style: ajustando controller

--- src/controller/funcionarios.controller.js
+++ src/controller/funcionarios.controller.js
@@ -1,17 +1,17 @@
-const FuncionarioService = require ('../services/funcionarios.service');
-const {request} = require ('express')
-class FuncionariosControler {
+const FuncionariosService = require ('../services/funcionarios.service');
+
+class FuncionariosController {
         constructor () {}
         async getAll (req, res, next){
             try {
                 // busca os nomes
-                const result = await FuncionarioService.getAll();
+                const result = await FuncionariosService.getAll();
                 // verificar quantidade de resultados
                 if (result.length <= 0)
                   // retorna msg caso não tenha funcionarios  
                   return res
                     .status(200)
-                    .json({ success: true, data: 'Nenhum Funcionario encontrado' });
+                    .json({ success: true, data: 'Nenhum Funcionarios encontrado' });
                 // retorna os resultados
                 return res.status(201).json({ success: true, data: result });
               } catch (error) {
@@ -25,7 +25,7 @@ class FuncionariosControler {
           // post cadastro de funcionário
         async create(req, res, next) {
             try {
-                const result = await FuncionarioService.create(req.body);
+                const result = await FuncionariosService.create(req.body);
             // verificar se o produto foi criado
                 if (!result) return res.status(201).json({ success: true, data: 'Houve um erro' });
             // retorna os resultados
@@ -38,12 +38,27 @@ class FuncionariosControler {
             }
         }
 
+            async update(req, res, next) {
+              try {
+              const {id}= req.params;
+              const result = await FuncionariosService.update(id ,req.body);
+              // verificar se Funcionario foi atualizado
+              if (!result) return res.status(201).json({ success: true, data: 'Cliente não encontrado' });
+              // retorna os resultados
+              return res.status(201).json({ success: true, data: result });
+              } catch (error) {
+              // imprime o erro no console
+              console.log(error);
+              // retorna erro caso ocorra algum erro
+              res.status(400).json({ success: false, data: error.message });
+              }
+          }        
 
           // Excluir Funcionário 
          async delete(req, res, next) {
             try {
             const {id}= req.params;
-            const result = await FuncionarioService.delete(id);
+            const result = await FuncionariosService.delete(id);
             // verificar se o funcionário foi deletado
             if (!result) return res.status(201).json({ success: true, data: 'Nenhum Funcionário com esse id foi encontrado' });
             // retorna os resultados
@@ -56,24 +71,29 @@ class FuncionariosControler {
     }
   }
 
-        async update(req, res, next) {
-            try {
-            const {id}= req.params;
-            const result = await FuncionarioService.update(id ,req.body);
-            // verificar se Funcionario foi atualizado
-            if (!result) return res.status(201).json({ success: true, data: 'Cliente não encontrado' });
-            // retorna os resultados
-            return res.status(201).json({ success: true, data: result });
-            } catch (error) {
-            // imprime o erro no console
-            console.log(error);
-            // retorna erro caso ocorra algum erro
-            res.status(400).json({ success: false, data: error.message });
-            }
-        }
 
+            // findByID produto
+            async findById(req, res, next) {
+              try {
+                const {id}= req.params;
+                const result = await FuncionariosService.findByID(id);
+                // verificar se o produto foi deletado
+                if (!result) return res.status(201).json({ success: true, data: 'Nenhum produto encontrado' });
+                // retorna os resultados
+                return res.status(201).json({ success: true, data: result });
+              } catch (error) {
+                // imprime o erro no console
+                console.log(error);
+                // retorna erro caso ocorra algum erro
+                res.status(400).json({ success: false, data: error.message });
+              }
+            }
+          
+                    
+
+      
 }
 
 
-const funcionariosControler = new FuncionariosControler();
-module.exports = funcionariosControler;
+const funcionariosController = new FuncionariosController();
+module.exports = funcionariosController;
